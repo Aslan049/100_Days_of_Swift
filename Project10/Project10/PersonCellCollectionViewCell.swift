7c2407ace449103d1dd61e3333222f78594b9c75
Aslan049 <aslankorkmaz049@gmail.com>
2025-04-16 21:00:45 +0300

    Project10 part1

---
+++ Project10/Project10/PersonCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCellCollectionViewCell.swift
+//  Project10
+//
+//  Created by Aslan Korkmaz on 16.04.2025.
+//
+
+import UIKit
+
+class PersonCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    
+}
---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Aslan Korkmaz on 16.04.2025.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCellCollectionViewCell else {
+            fatalError("Unable to dequeue PersonCell.")
+            
+        }
+        cell.label.text = "Aslan Korkmaz"
+        return cell
+    }
+
+}
+

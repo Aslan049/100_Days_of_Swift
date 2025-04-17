e3bfc46035e1c2f80c735d1a26eb64506f599d0d
Aslan049 <aslankorkmaz049@gmail.com>
2025-04-17 23:33:23 +0300

    Update Project10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Aslan Korkmaz on 17.04.2025.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
